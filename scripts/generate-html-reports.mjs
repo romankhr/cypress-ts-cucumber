2d252b8783b58d6b69fdb50076aca25d38e318b0
arnold-simanjuntak <arnolddominggo19@gmail.com>
2023-05-03 23:40:21 +0700

    feat : check browser and operating system when generating report

--- scripts/generate-html-reports.mjs
+++ scripts/generate-html-reports.mjs
@@ -1,6 +1,23 @@
 import { generate } from "multiple-cucumber-html-reporter";
+import { config as configEnv } from "dotenv";
+
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
+configEnv();
 
 generate({
   jsonDir: "jsonlogs",
   reportPath: "./report/cucumber-html-report",
+  metadata: {
+    browser: {
+      name: process.env.CYPRESS_BROWSER,
+    },
+    device: isGithubActions ? "Github Actions" : "Local Machine",
+    platform: {
+      name: "Ubuntu",
+      version: process.env.GITHUB_ACTIONS
+        ? process.env.CYPRESS_UBUNTU__VERSION
+        : "20.04",
+    },
+  },
 });
