4c6be8a3d679cf4d5c413e450243effd1a8eda03
arnold-simanjuntak <arnolddominggo19@gmail.com>
2023-05-04 00:40:18 +0700

    refactor : programmatically determine operating system when generating report

--- scripts/generate-html-reports.mjs
+++ scripts/generate-html-reports.mjs
@@ -1,22 +1,36 @@
 import { generate } from "multiple-cucumber-html-reporter";
 import { config as configEnv } from "dotenv";
+import { promisify } from "util";
+import getOs from "getos";
+const getOsPromisified = promisify(getOs);
 
 const isGithubActions = process.env.GITHUB_ACTIONS === "true";
 
 configEnv();
 
-generate({
-  jsonDir: "jsonlogs",
-  reportPath: "./report/cucumber-html-report",
-  metadata: {
-    browser: {
-      name: process.env.CYPRESS_BROWSER,
-      version: process.env.CYPRESS_BROWSER_VERSION || "XX.xx",
+async function getOsInfo() {
+  try {
+    const os = await getOsPromisified();
+    return os;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getOsInfo().then((info) => {
+  generate({
+    jsonDir: "jsonlogs",
+    reportPath: "./report/cucumber-html-report",
+    metadata: {
+      browser: {
+        name: process.env.CYPRESS_BROWSER,
+        version: process.env.CYPRESS_BROWSER_VERSION || "XX.xx",
+      },
+      device: isGithubActions ? "Github Actions" : "Local Machine",
+      platform: {
+        name: info.dist,
+        version: info.release,
+      },
     },
-    device: isGithubActions ? "Github Actions" : "Local Machine",
-    platform: {
-      name: "Ubuntu",
-      version: isGithubActions ? process.env.CYPRESS_UBUNTU__VERSION : "20.04",
-    },
-  },
+  });
 });
