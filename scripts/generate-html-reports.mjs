190c9006519932a8f7a44b4ea8dfaf4e1eb58ef7
arnold-simanjuntak <arnolddominggo19@gmail.com>
2023-05-03 23:49:38 +0700

    feat : check browser version

--- scripts/generate-html-reports.mjs
+++ scripts/generate-html-reports.mjs
@@ -5,12 +5,15 @@ const isGithubActions = process.env.GITHUB_ACTIONS === "true";
 
 configEnv();
 
+console.log("Browser version:", process.env.CYPRESS_BROWSER_VERSION);
+
 generate({
   jsonDir: "jsonlogs",
   reportPath: "./report/cucumber-html-report",
   metadata: {
     browser: {
       name: process.env.CYPRESS_BROWSER,
+      version: process.env.CYPRESS_BROWSER_VERSION || "XX.xx",
     },
     device: isGithubActions ? "Github Actions" : "Local Machine",
     platform: {
